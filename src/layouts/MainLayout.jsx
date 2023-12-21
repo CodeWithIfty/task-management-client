abec25ead055e07c4760edc41901a650d4e0f13b
CodeWithIftekhar <codewithiftekhar@gmail.com>
2023-12-21 12:59:21 +0600

    initialize react project using vite and setup react router and setup folder structure

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+const MainLayout = () => {
+  return (
+    <div>
+      Navbar
+      <Outlet />
+      Footer
+    </div>
+  );
+};
+
+export default MainLayout;
