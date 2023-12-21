7f781b0f38b2f2a27a1c105448ac3a483669b1b5
CodeWithIftekhar <codewithiftekhar@gmail.com>
2023-12-21 14:27:55 +0600

    added why needs WorkSync.Hub section

---
+++ src/components/Home/BuiltForSection/BuiltForSection.jsx
@@ -0,0 +1,167 @@
+const BuiltForSection = () => {
+  return (
+    <section className="py-16">
+      <div className="">
+        <h1 className="text-5xl text-center font-semibold">
+          Why Every Leader Needs <span className="text-primary">WorkSync.</span>
+          Hub
+        </h1>
+      </div>
+      <div className="flex justify-center mx-5 flex-col md:flex-row items-center gap-20 py-24 ">
+        <div className="">
+          <h1 className="text-4xl font-semibold mb-5">
+            Build a more productive and <br /> efficient company
+          </h1>
+          <ul className="text-xl font-medium space-y-3">
+            <li className="flex items-center gap-1">
+              <svg
+                fill="none"
+                viewBox="0 0 32 32"
+                height="25"
+                width="25"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle fill="white" r="16" cy="16" cx="16"></circle>
+                <circle
+                  fillOpacity="0.2"
+                  fill="#00E676"
+                  r="16"
+                  cy="16"
+                  cx="16"
+                ></circle>
+                <path
+                  fill="#00E676"
+                  d="M23.6652 10.5854C24.1116 11.0318 24.1116 11.7533 23.6652 12.1998L14.5218 21.3431C14.0753 21.7896 13.3539 21.7896 12.9074 21.3431L8.33473 16.7714C7.88842 16.325 7.88842 15.6035 8.33473 15.1571C8.78111 14.7106 9.50473 14.7106 9.95118 15.1571L13.6825 18.918L22.0508 10.5854C22.4972 10.1382 23.2187 10.1382 23.6652 10.5854Z"
+                ></path>
+              </svg>
+              Focus on what matters
+            </li>
+            <li className="flex items-center gap-1">
+              <svg
+                fill="none"
+                viewBox="0 0 32 32"
+                height="25"
+                width="25"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle fill="white" r="16" cy="16" cx="16"></circle>
+                <circle
+                  fillOpacity="0.2"
+                  fill="#00E676"
+                  r="16"
+                  cy="16"
+                  cx="16"
+                ></circle>
+                <path
+                  fill="#00E676"
+                  d="M23.6652 10.5854C24.1116 11.0318 24.1116 11.7533 23.6652 12.1998L14.5218 21.3431C14.0753 21.7896 13.3539 21.7896 12.9074 21.3431L8.33473 16.7714C7.88842 16.325 7.88842 15.6035 8.33473 15.1571C8.78111 14.7106 9.50473 14.7106 9.95118 15.1571L13.6825 18.918L22.0508 10.5854C22.4972 10.1382 23.2187 10.1382 23.6652 10.5854Z"
+                ></path>
+              </svg>
+              Quickly know what’s happening
+            </li>
+            <li className="flex items-center gap-1">
+              <svg
+                fill="none"
+                viewBox="0 0 32 32"
+                height="25"
+                width="25"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle fill="white" r="16" cy="16" cx="16"></circle>
+                <circle
+                  fillOpacity="0.2"
+                  fill="#00E676"
+                  r="16"
+                  cy="16"
+                  cx="16"
+                ></circle>
+                <path
+                  fill="#00E676"
+                  d="M23.6652 10.5854C24.1116 11.0318 24.1116 11.7533 23.6652 12.1998L14.5218 21.3431C14.0753 21.7896 13.3539 21.7896 12.9074 21.3431L8.33473 16.7714C7.88842 16.325 7.88842 15.6035 8.33473 15.1571C8.78111 14.7106 9.50473 14.7106 9.95118 15.1571L13.6825 18.918L22.0508 10.5854C22.4972 10.1382 23.2187 10.1382 23.6652 10.5854Z"
+                ></path>
+              </svg>
+              Make informed decisions
+            </li>
+            <li className="flex items-center gap-1">
+              <svg
+                fill="none"
+                viewBox="0 0 32 32"
+                height="25"
+                width="25"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle fill="white" r="16" cy="16" cx="16"></circle>
+                <circle
+                  fillOpacity="0.2"
+                  fill="#00E676"
+                  r="16"
+                  cy="16"
+                  cx="16"
+                ></circle>
+                <path
+                  fill="#00E676"
+                  d="M23.6652 10.5854C24.1116 11.0318 24.1116 11.7533 23.6652 12.1998L14.5218 21.3431C14.0753 21.7896 13.3539 21.7896 12.9074 21.3431L8.33473 16.7714C7.88842 16.325 7.88842 15.6035 8.33473 15.1571C8.78111 14.7106 9.50473 14.7106 9.95118 15.1571L13.6825 18.918L22.0508 10.5854C22.4972 10.1382 23.2187 10.1382 23.6652 10.5854Z"
+                ></path>
+              </svg>
+              Align and organize your team
+            </li>
+            <li className="flex items-center gap-1">
+              <svg
+                fill="none"
+                viewBox="0 0 32 32"
+                height="25"
+                width="25"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle fill="white" r="16" cy="16" cx="16"></circle>
+                <circle
+                  fillOpacity="0.2"
+                  fill="#00E676"
+                  r="16"
+                  cy="16"
+                  cx="16"
+                ></circle>
+                <path
+                  fill="#00E676"
+                  d="M23.6652 10.5854C24.1116 11.0318 24.1116 11.7533 23.6652 12.1998L14.5218 21.3431C14.0753 21.7896 13.3539 21.7896 12.9074 21.3431L8.33473 16.7714C7.88842 16.325 7.88842 15.6035 8.33473 15.1571C8.78111 14.7106 9.50473 14.7106 9.95118 15.1571L13.6825 18.918L22.0508 10.5854C22.4972 10.1382 23.2187 10.1382 23.6652 10.5854Z"
+                ></path>
+              </svg>
+              Engage and inspire
+            </li>
+            <li className="flex items-center gap-1">
+              <svg
+                fill="none"
+                viewBox="0 0 32 32"
+                height="25"
+                width="25"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle fill="white" r="16" cy="16" cx="16"></circle>
+                <circle
+                  fillOpacity="0.2"
+                  fill="#00E676"
+                  r="16"
+                  cy="16"
+                  cx="16"
+                ></circle>
+                <path
+                  fill="#00E676"
+                  d="M23.6652 10.5854C24.1116 11.0318 24.1116 11.7533 23.6652 12.1998L14.5218 21.3431C14.0753 21.7896 13.3539 21.7896 12.9074 21.3431L8.33473 16.7714C7.88842 16.325 7.88842 15.6035 8.33473 15.1571C8.78111 14.7106 9.50473 14.7106 9.95118 15.1571L13.6825 18.918L22.0508 10.5854C22.4972 10.1382 23.2187 10.1382 23.6652 10.5854Z"
+                ></path>
+              </svg>
+              Measure satisfaction
+            </li>
+          </ul>
+          <button className="px-6 py-4 bg-primary text-white rounded-full mt-4 hover:bg-transparent hover:ring-2 hover:text-black transition-all ease-in-out duration-100 focus:scale-95">
+            Explore More
+          </button>
+        </div>
+        <div className="">
+          <img src="/images/21.png" alt="" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default BuiltForSection;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../components/Home/Banner";
+import BuiltForSection from "../components/Home/BuiltForSection/BuiltForSection";
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <BuiltForSection />
     </div>
   );
 };
